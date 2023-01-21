4aa9e7dc57b54b9e2a522d583d99dfd9ca0a67e5
abethel912 <abethel180@gmail.com>
2023-01-20 23:34:45 -0500

    Established database connection

--- server.js
+++ server.js
@@ -4,11 +4,27 @@
 // .env variables
 require('dotenv').config()
 // pull PORT from .env, give default value of 4001
-const { PORT = 4001 } = process.env
+const { PORT = 4001, DATABASE_URL } = process.env
 // import express
 const express = require('express')
 // create application object
 const app = express()
+// import mongoose
+const mongoose = require("mongoose");
+
+// DATABASE CONNECTION
+
+// Establish Connection
+mongoose.connect(DATABASE_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("Your are connected to mongoose"))
+  .on("close", () => console.log("Your are disconnected from mongoose"))
+  .on("error", (error) => console.log(error));
 
 
 // ROUTES
