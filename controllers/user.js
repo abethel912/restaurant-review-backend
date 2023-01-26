08d4291577f4e822afd3bd4f7754edd852880501
Alec Hannaford <alec322@gmail.com>
2023-01-26 15:42:04 -0800

    user routes in controller

--- controllers/user.js
+++ controllers/user.js
@@ -5,15 +5,42 @@ const router = Router()
 const jwt = require('jsonwebtoken')
 const { useResolvedPath } = require('react-router-dom')
 const secret = process.env.secret
+const User = require('../model/user')
 
 router.get('/', (req,res) => {
     res.json({message: "WELCOME TO AUTH ROUTER"})
 })
 
-// router.post("/signup", async(req,res)=>{
-//     try{
-//         req.body.password = await bcrypt.hash(req.body.password, 10)
-//     }
-// })
+router.post("/signup", async(req,res)=>{
+    try{
+        req.body.password = await bcrypt.hash(req.body.password, 10)
+        const newUser = await User.create(req.body)
+        res.status(200).json(newUser)
+    }
+    catch(error){
+        res.status(400).json({error})
+    }
+})
+
+router.post('/login', async(req,res)=>{
+    try{
+        const {username, password} = req.body
+        const user = await User.findOne({username})
+        if (user){
+            const match = await bcrypt.compare(password, user.password)
+            if(match){
+                const token = await jwt.sign({username}, secret)
+                res.status(200).json({token})
+            }else{
+                res.status(400).json({error: "password does not match"})
+            }
+        }else{
+            res.status(400).json({error: "USER DOES NOT EXIST"})
+        }
+    }
+    catch(error){
+        res.status(400).json({error})
+    }
+})
 
 module.exports = router
