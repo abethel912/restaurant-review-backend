cbe2576fe7c12bcc5e04b1c108593a4e53b27f62
abethel912 <abethel180@gmail.com>
2023-01-26 23:01:46 -0500

    Added rating to schema

--- model/restaurant.js
+++ model/restaurant.js
@@ -5,7 +5,8 @@ const RestaurantSchema = new mongoose.Schema({
     name: String,
     image: String,
     address: String,
-    type: String
+    type: String,
+    rating: Number
 });
 
 const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
